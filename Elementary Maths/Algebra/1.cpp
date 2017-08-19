75cd948031b9547bfb77baa3be6ce78e42daf8e2
Chaitanya Tejaswi <aeronfinium@gmail.com>
2017-08-19 18:07:34 +0530

    Update 1.cpp

--- Elementary Maths/Algebra/1.cpp
+++ Elementary Maths/Algebra/1.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 /* Function Declarations */
-void inputMatrix (int MatX[][n],int m, int n0);
-void outputMatrix (int MatX[][n],int m, int n0);
-void addMatrix (int A[][n],int B[][n],int C[][n],int m, int n0);
+void matrix_in (int MatX[][n],int m, int n0);
+void matrix_out (int MatX[][n],int m, int n0);
+void matrix_add (int A[][n],int B[][n],int C[][n],int m, int n0);
 
 main(){
     /* Input Matrix Dimensions */
@@ -18,24 +18,24 @@ main(){
     int MatA[m][n],MatB[m][n],MatC[m][n];
     /* Input Matrix Elements */
     cout<<"############### Matrix A ###############"<<endl;
-    inputMatrix(MatA,m,n0);
+    matrix_in(MatA,m,n0);
     cout<<"############### Matrix B ###############"<<endl;
-    inputMatrix(MatB,m,n0);
+    matrix_in(MatB,m,n0);
     /* Compute & Display Result */
-    addMatrix(MatA,MatB,MatC,m,n0);
+    matrix_add(MatA,MatB,MatC,m,n0);
     cout<<"########################################"<<endl;
-    outputMatrix(MatC,m,n0);
+    matrix_out(MatC,m,n0);
     getch();
 }
 /* Function Definitions */
-void inputMatrix (int MatX[][n],int m, int n0){
+void matrix_in (int MatX[][n],int m, int n0){
         for (int i = 0; i<m; i++)
             for (int j = 0; j<n0; j++){
                 cout<<"Element["<<i<<","<<j<<"] Value: ";
                 cin>>MatX[i][j];
             }
 }
-void outputMatrix (int MatX[][n],int m, int n0){
+void matrix_out (int MatX[][n],int m, int n0){
     cout<<"########### Resultant Matrix ###########"<<endl;
     for (int i = 0; i<m; i++){
         for (int j = 0; j<n0; j++)
@@ -43,7 +43,7 @@ void outputMatrix (int MatX[][n],int m, int n0){
         cout<<endl;
     }
 }
-void addMatrix (int A[][n],int B[][n],int C[][n],int m, int n0){
+void matrix_add (int A[][n],int B[][n],int C[][n],int m, int n0){
     for (int i = 0; i<n; i++)
         for (int j = 0; j<n; j++)
                 C[i][j] = A[i][j] + B[i][j] ;
@@ -55,7 +55,7 @@ Note:
     where user chooses dimensions of matrices (m x n0) to work upon.
     This is because we have to initialize a matrix as A[][n] atleast. So we need atleast one parameter to be fixed for the compiler to allocate space.
  2. Also, no Array values are returned explicitly.
-     void inputMatrix (...)
-     void outputMatrix (...)
-     void addMatrix (...)
+     void matrix_in (...)
+     void matrix_out (...)
+     void matrix_add (...)
 */`

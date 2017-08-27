35138fc614d78d6e9569e1074c57cb27571987ca
Chaitanya Tejaswi <aeronfinium@gmail.com>
2017-08-28 01:31:22 +0530

    Update 01.c

--- Assembly/01.c
+++ Assembly/01.c
@@ -2,34 +2,35 @@
 #include <stdio.h>
 
 main(){
-	int arg1,arg2;
-	int add,sub,mul,quo,rem ;
+    int arg1,arg2;
+    int add,sub,mul,quo,rem ;
     printf("Enter Two Integers: ");
     scanf("%d%d",&arg1,&arg2);
 
     /* Addition */
     __asm__ ("addl %%ebx,%%eax;"
-			:"=a"(add)
-			:"a"(arg1),"b"(arg2)
-			);
-	/* Subtraction */
+            :"=a"(add)
+            :"a"(arg1),"b"(arg2)
+            );
+    /* Subtraction */
     __asm__ ("subl %%ebx,%%eax;"
-			:"=a"(sub)
-			:"a"(arg1),"b"(arg2)
-			);
-	/* Multiplication */
+            :"=a"(sub)
+            :"a"(arg1),"b"(arg2)
+            );
+    /* Multiplication */
     __asm__ ("imull %%ebx,%%eax;"
-			:"=a"(mul)
-			:"a"(arg1),"b"(arg2)
-			);
-	/* Division */
-	__asm__ ("movl $0,%%edx;"
+            :"=a"(mul)
+            :"a"(arg1),"b"(arg2)
+            );
+    /* Division */
+    __asm__ ("movl $0,%%edx;"
              "movl %2,%%eax;"
              "movl %3,%%ebx;"
              "idivl %%ebx;"
-			:"=&a"(quo),"=&d"(rem)
-			:"g"(arg1),"g"(arg2)
-			);
+            :"=&a"(quo),"=&d"(rem)
+            :"g"(arg1),"g"(arg2)
+            );
+	
     printf("%d + %d = %d\n",arg1,arg2,add);
     printf("%d - %d = %d\n",arg1,arg2,sub);
     printf("%d * %d = %d\n",arg1,arg2,mul);

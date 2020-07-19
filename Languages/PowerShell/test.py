b0b27639dd1dc5ae40324769018f98b22f79f4d6
CRTejaswi <aeronfinium@gmail.com>
2020-07-19 23:08:48 +0530

    JS: Updated 'SVG' (Shapes)

---
+++ Languages/PowerShell/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import pyodbc
+
+'''
+SQLServer
+https://towardsdatascience.com/sql-server-with-python-679b8dba69fa
+https://www.youtube.com/watch?v=eDXX5evRgQw
+
+Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;
+'''
